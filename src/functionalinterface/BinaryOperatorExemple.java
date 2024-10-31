e4ad046cec781350a3fbf6065f4952025a537d20
vitorwmoutinho <vitor.wenderson@gmail.com>
2024-10-31 15:48:35 -0300

    Encerrando o binary e montandos os desafios.

--- src/functionalinterface/BinaryOperatorExemple.java
+++ src/functionalinterface/BinaryOperatorExemple.java
@@ -1,14 +1,23 @@
 package functionalinterface;
 
-import java.util.ArrayList;
+
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.BinaryOperator;
 
-public class BinaryOperator {
+public class BinaryOperatorExemple {
     public static void main(String[] args) {
-        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5);
+        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 5);
 
-        BinaryOperator<Integer> somar = ;
+        BinaryOperator<Integer> somar = (num1, num2) -> num1 + num2;
+
+        int resultado = numeros.stream().reduce(0, Integer::sum);
+
+        System.out.println("Soma é: "+ resultado);
+
+
+
+
 
 
     }
