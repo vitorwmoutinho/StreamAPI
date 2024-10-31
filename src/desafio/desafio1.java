533ed725c056bef4aaf7995b4820b018ff896021
vitorwmoutinho <vitor.wenderson@gmail.com>
2024-10-31 16:09:21 -0300

    Desafio 1 - challenge completed!

--- src/desafio/desafio1.java
+++ src/desafio/desafio1.java
@@ -1,4 +1,22 @@
 package desafio;
 
+//Desafio 1 - Mostre a lista na ordem numérica:
+//Crie um programa que utilize a Stream API para ordenar a lista de números em ordem crescente
+// e a exiba no console.
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.stream.Collectors;
+
 public class desafio1 {
+    public static void main(String[] args) {
+        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
+
+        List<Integer> numerosOrdenados = numeros.stream().sorted().collect(Collectors.toList());
+
+        System.out.println("lista ordenanda: "+ numerosOrdenados);
+
+
+    }
 }
