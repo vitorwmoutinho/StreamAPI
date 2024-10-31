98dfdefc2629a26a6969d4eae59639d3ed1aeb6a
vitorwmoutinho <vitor.wenderson@gmail.com>
2024-10-31 15:14:40 -0300

    Aprendendo sobre function com exemplos praticos.

--- src/functionalinterface/FunctionExample.java
+++ src/functionalinterface/FunctionExample.java
@@ -5,18 +5,22 @@ import java.util.List;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
+import static java.util.stream.Collectors.toList;
+
 public class FunctionExample {
     public static void main(String[] args) {
         List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5);
 
         Function<Integer, Integer> dobrar = numero -> numero * 2;
 
-        List<Integer> numerodobrados= numeros.stream().map(dobrar).collect(Collectors.toList());
+        List<Integer> numerodobrados= numeros.stream()
+                .map(n -> n * 2).toList();
 
 
-
+        numerodobrados.forEach(System.out::println);
 
 
 
     }
+
 }
