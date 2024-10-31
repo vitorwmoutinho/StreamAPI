3745fe30677b56e6f4b4232774ffcfc38b7e2158
vitorwmoutinho <vitor.wenderson@gmail.com>
2024-10-31 15:07:09 -0300

    Exemplos da Utilização do function

--- src/functionalinterface/FunctionExample.java
+++ src/functionalinterface/FunctionExample.java
@@ -1,4 +1,22 @@
 package functionalinterface;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
 public class FunctionExample {
+    public static void main(String[] args) {
+        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5);
+
+        Function<Integer, Integer> dobrar = numero -> numero * 2;
+
+        List<Integer> numerodobrados= numeros.stream().map(dobrar).collect(Collectors.toList());
+
+
+
+
+
+
+    }
 }
