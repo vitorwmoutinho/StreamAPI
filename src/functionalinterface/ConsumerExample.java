930b2352c73850a39be81a1f5b159d1361eb4342
vitorwmoutinho <vitor.wenderson@gmail.com>
2024-10-31 15:28:25 -0300

    Atualizando Consumer e com o filter aprendido em Predicate

--- src/functionalinterface/ConsumerExample.java
+++ src/functionalinterface/ConsumerExample.java
@@ -15,11 +15,7 @@ public class ConsumerExample {
             }
         };
 
-        numeros.forEach(n -> {
-                if(n % 2 == 0){
-                    System.out.println(n);
-                }
-            }
-        );
+        numeros.stream().filter(n -> n % 2 == 0).forEach(System.out::println);
+
     }
 }
--- src/functionalinterface/PredicateExample.java
+++ src/functionalinterface/PredicateExample.java
@@ -1,4 +1,21 @@
 package functionalinterface;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+
 public class PredicateExample {
+    public static void main(String[] args) {
+
+
+    List<String> palavras = Arrays.asList("java","Kotlin","python"
+            ,"javascript", "c", "go", "ruby");
+
+    Predicate<String> maisDeCincoCaracteres = palavra -> palavra.length() > 5;
+
+    palavras.stream().filter(p -> {
+            return p.length() < 5;
+            }).forEach(System.out::println);
+
+    }
 }
