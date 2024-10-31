3ad48551f6c83bfadafc3851c377d9d01002dd1a
vitorwmoutinho <vitor.wenderson@gmail.com>
2024-10-31 15:01:24 -0300

    Function<T, R>: Representa uma função que aceita um argumento do tipo T e retorna um resultado do tipo R. É utilizada para transformar ou mapear os elementos do Stream em outros valores ou tipos.

--- src/functionalinterface/SupplierExample.java
+++ src/functionalinterface/SupplierExample.java
@@ -1,4 +1,20 @@
 package functionalinterface;
 
+import java.util.List;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class SupplierExample {
+    public static void main(String[] args) {
+        Supplier<String> saudacao = () -> "Olá, seja bem vindo";
+
+        List<String> listasaudacoes = Stream.generate(() -> "Olá, seja bem vindo")
+                .limit(3).toList();
+
+        listasaudacoes.forEach(System.out::println);
+
+    }
+
+
 }
