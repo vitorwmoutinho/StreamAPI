fb1d5a6b717858b643884258e2208ee32b10575e
vitorwmoutinho <vitor.wenderson@gmail.com>
2024-11-01 11:31:19 -0300

    Verificação do código do deasfio 2 e implementação do desafio 3.

--- src/desafio/Desafio2.java
+++ src/desafio/Desafio2.java
@@ -14,11 +14,11 @@ public class Desafio2 {
     public static void main(String[] args) {
         List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
 
-        Consumer<Integer> imprimirNumeroPar = numero -> {
-            if(numero % 2 == 0){
-                System.out.println(numero);
-            }
-        };
+        //Consumer<Integer> imprimirNumeroPar = numero -> {
+            //if(numero % 2 == 0){
+                //System.out.println(numero);
+           // }
+        //};
 
 
         int somaPares = numeros.stream().filter(n -> n % 2 == 0).mapToInt(Integer::intValue).sum();
--- src/desafio/desafio3.java
+++ src/desafio/desafio3.java
@@ -1,4 +1,13 @@
 package desafio;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class desafio3 {
+    public static void main(String[] args) {
+        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
+
+
+
+    }
 }
