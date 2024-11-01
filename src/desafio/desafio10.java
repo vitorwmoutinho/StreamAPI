6eff10a2af1f1c3fd716d50eaefd21b3f7f03850
vitorwmoutinho <vitor.wenderson@gmail.com>
2024-11-01 15:31:58 -0300

    Implementação e teste do código.

--- src/desafio/desafio10.java
+++ src/desafio/desafio10.java
@@ -1,4 +1,24 @@
 package desafio;
+/*
+Desafio 10 - Agrupe os valores ímpares múltiplos de 3 ou de 5:
+Utilize a Stream API para agrupar os valores ímpares múltiplos de 3 ou de 5 e exiba
+o resultado no console.
+ */
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class desafio10 {
+    public static void main(String[] args){
+        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
+
+        List<Integer> imparesMultiplosDe3Ou5 = numeros.stream()
+                .filter(n -> n % 2 != 0)
+                .filter(n -> n % 3 == 0 || n % 5 == 0)
+                .collect(Collectors.toList());
+
+        System.out.println("Numeros ímpares multiplos de 3 ou 5: "+ imparesMultiplosDe3Ou5);
+
+
+    }
 }
