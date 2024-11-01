777d7db0682801eb1143ea2dde61bdf539697a90
vitorwmoutinho <vitor.wenderson@gmail.com>
2024-11-01 15:47:02 -0300

    Criação do código e implementação do desafio 12

--- src/desafio/desafio12.java
+++ src/desafio/desafio12.java
@@ -1,5 +1,7 @@
 package desafio;
-/*
+/*]Desafio 12 - Encontre o produto de todos os números da lista:
+Com a ajuda da Stream API, encontre o produto de todos os números da lista e exiba
+o resultado no console.
 
  */
 import java.util.Arrays;
@@ -8,6 +10,12 @@ import java.util.List;
 public class desafio12 {
     public static void main(String[] args){
         List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
+
+        int produtosNumeros = numeros.stream().reduce(1,(a, b) -> a * b);
+
+        System.out.println("Produtos do número: "+ produtosNumeros);
+
+
 
     }
 }
