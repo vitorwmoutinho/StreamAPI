2d3f9eefca54d8cfcc99a2800f0ef24d213c93fd
vitorwmoutinho <vitor.wenderson@gmail.com>
2024-11-01 13:39:33 -0300

    Implementando os parametros e verifcansua lógica

--- src/desafio/desafio6.java
+++ src/desafio/desafio6.java
@@ -1,4 +1,14 @@
 package desafio;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class desafio6 {
+    public static void main(String [] args){
+        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
+
+        boolean maiorQueDez = numeros.stream().anyMatch(n -> n > 10);
+
+        System.out.println("Maior que dez: " + maiorQueDez);
+    }
 }
