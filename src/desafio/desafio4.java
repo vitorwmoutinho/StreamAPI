f4bf76909ce750799bef67007bf39e6a3c4b2ea2
vitorwmoutinho <vitor.wenderson@gmail.com>
2024-11-01 11:53:38 -0300

    Desafio 4 completo.

--- src/desafio/desafio4.java
+++ src/desafio/desafio4.java
@@ -1,13 +1,23 @@
 package desafio;
-
+/*
+esafio 4 - Remova todos os valores ímpares:
+Utilize a Stream API para remover os valores ímpares da lista e imprima a lista resultante no console.
+ */
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class desafio4 {
     public static void main(String[] args) {
         List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
 
-        numeros.stream().filter(n -> n % 2 == 0).forEach(System.out::println);
-        
+        List<Integer> numerosPares = numeros.stream()
+                .filter(n -> n % 2 == 0)
+                .collect(Collectors.toList());
+
+        System.out.println(numerosPares);
+
+
+
     }
 }
