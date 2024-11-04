2112d43347ef246f7c85510947d3e63e0c476584
vitorwmoutinho <vitor.wenderson@gmail.com>
2024-11-04 13:52:05 -0300

    implementando os metodos do optional

--- src/optional/orElseSuplier.java
+++ src/optional/orElseSuplier.java
@@ -1,4 +1,11 @@
 package optional;
 
+import java.util.Optional;
+
 public class orElseSuplier {
+    public static void main(String[] args) {
+        Optional<String> optionalValue = Optional.ofNullable(null);
+        String result = optionalValue.orElseGet(() -> "Value from Supplier");
+        System.out.println(result);
+    }
 }
