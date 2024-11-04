b5ecccdbc28f14de7af0bfa66c7baca06c4b5320
vitorwmoutinho <vitor.wenderson@gmail.com>
2024-11-04 13:29:28 -0300

    Implementação do código

--- src/optional/orElseDefaultValue.java
+++ src/optional/orElseDefaultValue.java
@@ -1,4 +1,11 @@
 package optional;
 
+import java.util.Optional;
+
 public class orElseDefaultValue {
+    public static void main(String[] args) {
+        Optional<String> optionalValue = Optional.ofNullable(null);
+        String result = optionalValue.orElse("Default");
+        System.out.println(result);
+    }
 }
